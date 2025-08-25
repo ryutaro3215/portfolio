a816a14ce18a9d50a60b0fb73b8ade4a1f8cef6c
rmatsuba <87314627+ryutaro3215@users.noreply.github.com>
2025-08-26 03:12:17 +0900

    reset project

--- src/entry-client.tsx
+++
@@ -1,9 +0,0 @@
-import { hydrateRoot, createRoot } from "react-dom/client";
-import App from "./App";
-
-const container = document.getElementById("root")!;
-if (container.hasChildNodes()) {
-  hydrateRoot(container, <App />);
-} else {
-  createRoot(container).render(<App />);
-}
--- src/entry-server.tsx
+++
@@ -1,7 +0,0 @@
-import { renderToString } from "react-dom/server";
-import App from "./App";
-
-export async function render(/* url: string */) {
-  const appHtml = renderToString(<App />);
-  return { appHtml, head: "" };
-}
